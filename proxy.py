437e274e671fb7e192808e9465fa16c09cce6aba
= <sean.fioritto@gmail.com>
2012-03-17 23:50:14 -0500

    if the article is less than 250, just fall back to the original.

--- proxy.py
+++ proxy.py
@@ -13,6 +13,7 @@ class ProxyClient(proxy.ProxyClient):
         self.buffer = ""
         self.text = False
 
+
     def handleHeader(self, key, value):
         if key == "Content-Type" and value.startswith("text/html"):
             self.text = True
@@ -25,27 +26,32 @@ class ProxyClient(proxy.ProxyClient):
 
 
     def handleResponseEnd(self):
-        headers = dict(self.father.requestHeaders.getAllRawHeaders())
-        if headers.has_key("content-encoding") and headers["content-encoding"] == "gzip":
-            sio = StringIO.StringIO(self.buffer)
-            gzipf = gzip.GzipFile(fileobj=sio, mode='r')
-            self.buffer = gzipf.read()
-            
-        if self.text:
-            try:
-                readable_article = Document(self.buffer).summary()
-                markup = readable_article
-                markup = markup.encode("utf-8")
-            except:
-                markup = self.buffer
+        
+        if not self._finished:
 
-        else:
-            markup = self.buffer
+            headers = dict(self.father.requestHeaders.getAllRawHeaders())
+            if headers.has_key("content-encoding") and headers["content-encoding"] == "gzip":
+                sio = StringIO.StringIO(self.buffer)
+                gzipf = gzip.GzipFile(fileobj=sio, mode='r')
+                self.buffer = gzipf.read()
+            if self.text:
+                try:
+                    readable_article = Document(self.buffer).summary()
+                    if len(readable_article) > 250:
+                        markup = readable_article
+                        markup = markup.encode("utf-8")
+                    else:
+                        markup = self.buffer
+                except:
+                    markup = self.buffer
+
+            else:
+                markup = self.buffer
                 
-        self.father.responseHeaders.setRawHeaders("content-length", [len(markup)])
-        self.father.write(markup)
+            self.father.responseHeaders.setRawHeaders("content-length", [len(markup)])
+            self.father.write(markup)
             
-        proxy.ProxyClient.handleResponseEnd(self)
+            proxy.ProxyClient.handleResponseEnd(self)
 
 
 class ProxyClientFactory(proxy.ProxyClientFactory):
