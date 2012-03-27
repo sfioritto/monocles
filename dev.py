e6904180e193155262664c81228c80360e5bd0e9
= <sean.fioritto@gmail.com>
2012-03-27 12:39:55 -0500

    added factory protocol in prod file.

--- dev.py
+++ dev.py
@@ -1,5 +1,9 @@
+from twisted.web import http
 from twisted.internet import reactor
-from monocles.proxy import factory
+from monocles.proxy import Proxy
+
+factory = http.HTTPFactory()
+factory.protocol = Proxy
 
 reactor.listenTCP(8080, factory)
 reactor.run()
--- prod.py
+++ prod.py
@@ -1,6 +1,10 @@
+from twisted.web import http
 from twisted.application.internet import TCPServer
 from twisted.application.service import Application
-from monocles.proxy import factory
+from monocles.proxy import Proxy
+
+factory = http.HTTPFactory()
+factory.protocol = Proxy
 
 application = Application("monocles")
 service = TCPServer(80, factory)
