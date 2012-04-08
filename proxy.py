7a5976b0f24a9da6af6833ea496a5101605baaa1
= <sean.fioritto@gmail.com>
2012-04-08 10:35:44 -0500

    cleanup.

--- proxy.py
+++ proxy.py
@@ -29,17 +29,7 @@ class ProxyClient(proxy.ProxyClient):
     def __init__(self, *args, **kwargs):
         proxy.ProxyClient.__init__(self, *args, **kwargs)
         self.buffer = ""
-        self.text = False
         self.haveAllHeaders = False
-
-
-    def handleHeader(self, key, value):
-        #TODO: probably don't need to do this, can get headers on self.father
-
-        if key == "Content-Type" and should_parse(value.lower()):
-            self.text = True
-
-        proxy.ProxyClient.handleHeader(self, key, value)
 
 
     def dataReceived(self, data):
@@ -68,65 +58,56 @@ class ProxyClient(proxy.ProxyClient):
 
             #skip urls with a special query string
             bypass, loggit, boiler = helper_options(self.father.uri)
-            
             if bypass:
                 self.father.write(self.buffer)
                 if loggit:
                     log.msg("MONOCLES: %s" % self.father.uri)
 
                 return proxy.ProxyClient.handleResponseEnd(self)
+                
 
-            if is_gzipped(self.father.responseHeaders):
-                self.father.responseHeaders.removeHeader("content-encoding")
-                self.buffer = gunzip(self.buffer)
+            if should_parse(ctype.lower())
 
+                if is_gzipped(self.father.responseHeaders):
+                    self.father.responseHeaders.removeHeader("content-encoding")
+                    self.buffer = gunzip(self.buffer)
 
-            if self.text:
                 try:
                     if boiler:
                         extractor = Extractor(extractor='ArticleExtractor', html=self.buffer)
                         readable_article = extractor.getHTML()
                     else:
                         readable_article = Document(self.buffer).summary()
-                        
-                    #todo: better way to determine if you shouldn't try to beautify the document.
-                    if len(readable_article) > 250: 
-
-                        bypass, loggit, boiler = get_helper_urls(self.father.uri)
-                        markup = styled_markup(readable_article, bypass, loggit, boiler)
-
-                        #accept-charset was set to only utf-8, so assuming
-                        #the response is encoded as utf-8. we'll see how that works out...
-                        #todo: it didn't work out. sometimes you get no charset and you need to
-                        #default to what the http spec says you should. you'll have to look it up.
-                        markup = markup.encode("utf-8")
-                    else:
-                        markup = self.buffer
-
-                    encodings = self.father.requestHeaders.getRawHeaders("accept-encoding")
-                    if encodings and "gzip" in encodings[0]:
-                        self.father.responseHeaders.setRawHeaders("content-encoding", ["gzip"])
-                        sio = StringIO()
-                        gzf = gzip.GzipFile(fileobj=sio, mode="wb")
-                        gzf.write(markup)
-                        gzf.close()
-                        markup = sio.getvalue()
-
-
-                    self.father.responseHeaders.setRawHeaders("content-length", [len(markup)])
-                    self.father.responseHeaders.setRawHeaders("content-type", ["text/html; charset=utf-8"])
-
-                    #TODO: turn off after done developing, for now don't ever cache anything.
-                    self.father.responseHeaders.removeHeader("etag")
-                    self.father.responseHeaders.setRawHeaders("cache-control", ["no-store",
-                                                                                 "no-cache",
-                                                                                 "must-revalidate",
-                                                                                 "post-check=0",
-                                                                                 "pre-check=0"])
-
+                    markup = styled_markup(readable_article, bypass, loggit, boiler)
                 except:
                     markup = self.buffer
 
+
+                #todo: write an "encode" function, pass it the charset header (so content-type)
+                markup = markup.encode("utf-8")
+
+
+                #todo: write a gzipit function
+                encodings = self.father.requestHeaders.getRawHeaders("accept-encoding")
+                if encodings and "gzip" in encodings[0]:
+                    self.father.responseHeaders.setRawHeaders("content-encoding", ["gzip"])
+                    sio = StringIO()
+                    gzf = gzip.GzipFile(fileobj=sio, mode="wb")
+                    gzf.write(markup)
+                    gzf.close()
+                    markup = sio.getvalue()
+
+
+                self.father.responseHeaders.setRawHeaders("content-length", [len(markup)])
+                self.father.responseHeaders.setRawHeaders("content-type", ["text/html; charset=utf-8"])
+
+                #TODO: turn off after done developing, for now don't ever cache anything.
+                self.father.responseHeaders.removeHeader("etag")
+                self.father.responseHeaders.setRawHeaders("cache-control", ["no-store",
+                                                                             "no-cache",
+                                                                             "must-revalidate",
+                                                                             "post-check=0",
+                                                                             "pre-check=0"])
             else:
                 markup = self.buffer
 
